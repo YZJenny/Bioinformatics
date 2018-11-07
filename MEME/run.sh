19298e36b9524ddd11fc108a37c844c312525be8
Yan zijun <40624018+YZJenny@users.noreply.github.com>
2018-11-07 20:52:33 +0800

    Update run.sh

--- MEME/run.sh
+++ MEME/run.sh
@@ -1,8 +1,5 @@
-
-
-
-1. get fasta
+# 1. get fasta
 bedtools getfasta -fi /home/genomewide/refgenome/hg19/hg19.fa -bed pipeline/MEME/data/H3K27me3_peaks.broadPeak -fo pipeline/MEME/data/H3K27me3_peaks.fa &
 
-2. run meme
+# 2. run meme
 meme pipeline/MEME/data/H3K27me3_peaks.fa -o pipeline/MEME/output -dna &
