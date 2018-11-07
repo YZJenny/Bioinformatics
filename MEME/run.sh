2ea9de20e7d75699b788c337a7c18d5094e0621a
Yan zijun <40624018+YZJenny@users.noreply.github.com>
2018-11-07 20:59:44 +0800

    Update run.sh

--- MEME/run.sh
+++ MEME/run.sh
@@ -1,5 +1,25 @@
-# 1. get fasta
+# 1.Installation
+#1.1 Download the software from http://meme-suite.org/doc/download.html/
+#1.2 Type the following commands
+tar zxf meme_5.0.2.tar.gz
+cd meme_5.0.2
+./configure --prefix=$HOME/meme --with-url=http://meme-suite.org/ --enable-build-libxml2 --enable-build-libxslt
+make
+make test
+make install 
+export PATH=$HOME/meme/bin:$PATH 
+# 1.3 Prerequisite software
+#(1) Perl
+    cd scripts
+    perl dependencies.pl
+#(2) Python
+#(3) zlib
+#(4) Ghostscript - for creating PNG files.
+#(5) Assorted common utilities
+
+# 2. get fasta
 bedtools getfasta -fi /home/genomewide/refgenome/hg19/hg19.fa -bed pipeline/MEME/data/H3K27me3_peaks.broadPeak -fo pipeline/MEME/data/H3K27me3_peaks.fa &
 
-# 2. run meme
+# 3. run meme
 meme pipeline/MEME/data/H3K27me3_peaks.fa -o pipeline/MEME/output -dna &
+
