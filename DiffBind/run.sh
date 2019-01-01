2963f0e829dca3d1bdca0b891439032c4a803423
Yan zijun <40624018+YZJenny@users.noreply.github.com>
2019-01-01 14:11:20 +0800

    Create run.sh

---
+++ DiffBind/run.sh
@@ -0,0 +1,16 @@
+# 1. Installation
+enter R:
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+BiocManager::install("DiffBind", version = "3.8")
+
+# 2. processing overview
+# 2.1 Reading in the peaksets
+
+# 2.2 Counting reads
+
+# 2.3 Establishing a contrast
+
+# 2.4 Perferming the differential analysis
+
+# 2.5 Retrieving the differentially bound sites
