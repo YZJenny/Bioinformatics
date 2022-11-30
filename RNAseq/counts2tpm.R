6f7e7cd2775551a9cbbfa0c12aca711dd2c8e725
Yan zijun <40624018+YZJenny@users.noreply.github.com>
2022-11-30 10:54:13 +0800

    Update counts2tpm.R

--- RNAseq/counts2tpm.R
+++ RNAseq/counts2tpm.R
@@ -1,14 +1,19 @@
 rm(list=ls())
-setwd('/local/yanzijun/SY/data/221111/')
-load('T-ALL-5batch.RData')
-
 library(biomaRt)
+setwd('/mdshare/node9/yanzijun/')
+
+##################
+### 1. COUNT转TPM
+##################
+COUNT <- read.table('/mdshare/node9/yanzijun/SY/data/GSE141140_all.counts.exp.txt',header = T,sep='\t')
+rownames(COUNT) <- COUNT$Gene;COUNT$Gene=NULL
+
 mart=useMart('ensembl')
 listDatasets(mart)
 bmart <- biomaRt::useMart(biomart = 'ENSEMBL_MART_ENSEMBL',
                           dataset = 'hsapiens_gene_ensembl',
                           host='www.ensembl.org') #grch38.p13版本
-feature_ids <- rownames(adjusted_data)
+feature_ids <- rownames(COUNT)
 length(feature_ids)
 #listAttributes(bmart)
 attributes=c(
@@ -16,7 +21,7 @@ attributes=c(
   'hgnc_symbol','chromosome_name','start_position','end_position')
 filters='hgnc_symbol'
 #feature_info <- biomaRt::getBM(attributes = attributes,mart = bmart) 
-#length(intersect(unique(feature_info$hgnc_symbol),rownames(adjusted_data))) #20332
+#length(intersect(unique(feature_info$hgnc_symbol),rownames(COUNT))) #20332
 
 feature_info <- biomaRt::getBM(attributes = attributes,filters = filters,values = feature_ids,mart = bmart)
 dim(feature_info)
@@ -25,13 +30,17 @@ dim(feature_info)
 feature_info_full <- feature_info[!grepl('^CHR|^GL',feature_info$chromosome_name),]
 dim(feature_info_full)
 
-print(length(unique(feature_info_full$hgnc_symbol))) #其实还是有几十个重复的，忽略
+print(length(unique(feature_info_full$hgnc_symbol)))
 feature_info_full <- feature_info_full[!duplicated(feature_info_full$hgnc_symbol),]
 dim(feature_info_full)
 
-mm <- match(feature_info_full$hgnc_symbol,feature_ids)
-count <- adjusted_data[mm,]
-dim(count)
+ol <- intersect(feature_info_full$hgnc_symbol,feature_ids)
+feature_info_full <- feature_info_full[feature_info_full$hgnc_symbol %in% ol,]
+dim(feature_info_full)
+feature_info_full$start_position <- as.numeric(feature_info_full$start_position)
+feature_info_full$end_position <- as.numeric(feature_info_full$end_position)
+
+count <- as.data.frame(t(dplyr::select(as.data.frame(t(COUNT)),feature_info_full$hgnc_symbol)))
 print(all(rownames(count)==feature_info_full$hgnc_symbol))
 
 
@@ -43,5 +52,5 @@ eff_length2 <- feature_info_full[,c(1,5)]
 x <- as.data.frame(count/eff_length2$eff_length)
 tpm=t(t(x)/colSums(x))*1e6
 logTPM <- log(tpm+1,2)
-write.csv(logTPM,'logTPM_afterbatch.csv')
-save.image('T-ALL-5batch.RData')
+write.table(logTPM,file="/mdshare/node9/yanzijun/SY/data/GSE141140.ltpm.txt",sep="\t",quote=F)
+print(max(logTPM))
